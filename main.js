04546e5e8507910cfd77b3c3bac1257371cb958e
GFLJS2100-user <reyedtreefrog@gmail.com>
2025-01-02 09:50:45 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 const sampleRate = 61440;
 const code = '';
-const this.t = 0;
-const this.y = 0;
-const this.x = 0;
+let t = 0;
+let y = 0;
+let x = 0;
 
 function yuv2rgb(y, u, v) {
     // Convert YUV to RGB
@@ -16,3 +16,257 @@ function yuv2rgb(y, u, v) {
         Math.min(255, Math.max(0, Math.round(b * 255)))
     ];
 }
+
+class IBNIZEmulator {
+    constructor() {
+        this.stack = [];
+        this.memory = new Uint32Array(0x100000); // 1 megaword address space
+        this.T = 0; // Time counter
+    }
+
+    push(value) {
+        this.stack.push(value);
+    }
+
+    pop() {
+        return this.stack.pop();
+    }
+
+    executeInstruction(instr) {
+        switch (instr) {
+            case '+':
+                this.push(this.pop() + this.pop());
+                break;
+            case '-':
+                this.push(this.pop() - this.pop());
+                break;
+            case '*':
+                this.push(this.pop() * this.pop());
+                break;
+            case '/':
+                const b = this.pop();
+                const a = this.pop();
+                this.push(b === 0 ? 0 : a / b);
+                break;
+            case '%':
+                const b2 = this.pop();
+                const a2 = this.pop();
+                this.push(b2 === 0 ? 0 : a2 % b2);
+                break;
+            case 'q':
+                this.push(Math.sqrt(this.pop()));
+                break;
+            case '&':
+                this.push(this.pop() & this.pop());
+                break;
+            case '|':
+                this.push(this.pop() | this.pop());
+                break;
+            case '^':
+                this.push(this.pop() ^ this.pop());
+                break;
+            case 'r':
+                const b3 = this.pop();
+                const a3 = this.pop();
+                this.push((a3 >>> b3) | (a3 << (32 - b3)));
+                break;
+            case 'l':
+                const b4 = this.pop();
+                const a4 = this.pop();
+                this.push(a4 << b4);
+                break;
+            case '~':
+                this.push(~this.pop());
+                break;
+            case 's':
+                this.push(Math.sin(this.pop() * 2 * Math.PI));
+                break;
+            case 'a':
+                const b5 = this.pop();
+                const a5 = this.pop();
+                this.push(Math.atan2(a5, b5) / (2 * Math.PI));
+                break;
+            case '<':
+                const a6 = this.pop();
+                this.push(a6 < 0 ? a6 : 0);
+                break;
+            case '>':
+                const a7 = this.pop();
+                this.push(a7 > 0 ? a7 : 0);
+                break;
+            case '=':
+                this.push(this.pop() === 0 ? 1 : 0);
+                break;
+            case 'd':
+                const val = this.pop();
+                this.push(val);
+                this.push(val);
+                break;
+            case 'p':
+                this.pop();
+                break;
+            case 'x':
+                const b8 = this.pop();
+                const a8 = this.pop();
+                this.push(b8);
+                this.push(a8);
+                break;
+            case 'v':
+                const c = this.pop();
+                const b9 = this.pop();
+                const a9 = this.pop();
+                this.push(b9);
+                this.push(c);
+                this.push(a9);
+                break;
+            case ')':
+                const i = this.pop();
+                this.push(this.stack[this.stack.length - 1 - i]);
+                break;
+            case '(':
+                const val2 = this.pop();
+                const i2 = this.pop();
+                this.stack[this.stack.length - 2 - i2] = val2;
+                break;
+            case 'M':
+                // Media switch logic
+                this.T++;
+                break;
+            case 'w':
+                // Whereami logic
+                this.push(this.T);
+                this.push(this.y);
+                this.push(this.x);
+                break;
+            case 'T':
+                // Terminate logic
+                return;
+            case '@':
+                const addr = this.pop();
+                this.push(this.memory[addr]);
+                break;
+            case '!':
+                const val3 = this.pop();
+                const addr2 = this.pop();
+                this.memory[addr2] = val3;
+                break;
+            case '?':
+                const cond = this.pop();
+                if (cond === 0) {
+                    // Skip until 'else' or 'endif'
+                    while (instr !== ':' && instr !== ';') {
+                        instr = this.code[++this.instructionPointer];
+                    }
+                }
+                break;
+            case ':':
+                // Skip until after next 'endif'
+                while (instr !== ';') {
+                    instr = this.code[++this.instructionPointer];
+                }
+                break;
+            case ';':
+                // End of conditional block
+                break;
+            case 'X':
+                const i0 = this.pop();
+                this.rstack.push(i0);
+                this.rstack.push(this.instructionPointer);
+                break;
+            case 'L':
+                const i1 = this.rstack.pop() - 1;
+                this.rstack.push(i1);
+                if (i1 !== 0) {
+                    this.instructionPointer = this.rstack[this.rstack.length - 2];
+                }
+                break;
+            case 'i':
+                this.push(this.rstack[this.rstack.length - 1]);
+                break;
+            case 'j':
+                this.push(this.rstack[this.rstack.length - 3]);
+                break;
+            case '[':
+                this.rstack.push(this.instructionPointer);
+                break;
+            case ']':
+                const cond2 = this.pop();
+                if (cond2 !== 0) {
+                    this.instructionPointer = this.rstack[this.rstack.length - 1];
+                }
+                break;
+            case 'J':
+                this.instructionPointer = this.pop();
+                break;
+            case '{':
+                const subAddr = this.pop();
+                this.memory[subAddr] = this.instructionPointer;
+                while (instr !== '}') {
+                    instr = this.code[++this.instructionPointer];
+                }
+                break;
+            case '}':
+                this.instructionPointer = this.rstack.pop();
+                break;
+            case 'V':
+                const subAddr2 = this.pop();
+                this.rstack.push(this.instructionPointer);
+                this.instructionPointer = this.memory[subAddr2];
+                break;
+            case 'R':
+                this.push(this.rstack.pop());
+                break;
+            case 'P':
+                this.rstack.push(this.pop());
+                break;
+            case 'U':
+                // User input logic (to be implemented)
+                break;
+            case 'G':
+                const numBits = this.pop();
+                // Get data (to be implemented)
+                break;
+            case '$':
+                // Start data segment (to be implemented)
+                break;
+            default:
+                throw new Error(`Unknown instruction: ${instr}`);
+        }
+    }
+
+    runProgram(program) {
+        this.code = program.split('');
+        this.instructionPointer = 0;
+        while (this.instructionPointer < this.code.length) {
+            const instr = this.code[this.instructionPointer++];
+            this.executeInstruction(instr);
+        }
+    }
+}
+
+// Canvas setup
+const canvas = document.getElementById('ibniz-canvas');
+const ctx = canvas.getContext('2d');
+const imageData = ctx.createImageData(canvas.width, canvas.height);
+
+// Rendering loop at 60FPS
+function render() {
+    const emulator = new IBNIZEmulator();
+    const program = '^xp'; // Example program
+    emulator.runProgram(program);
+
+    for (let i = 0; i < imageData.data.length; i += 4) {
+        const y = (i / 4) % 256;
+        const x = Math.floor((i / 4) / 256);
+        const [r, g, b] = yuv2rgb(emulator.stack[0], 0, 0); // Replace with actual values
+        imageData.data[i] = r;
+        imageData.data[i + 1] = g;
+        imageData.data[i + 2] = b;
+        imageData.data[i + 3] = 255; // Alpha channel
+    }
+
+    ctx.putImageData(imageData, 0, 0);
+    requestAnimationFrame(render);
+}
+
+requestAnimationFrame(render);
